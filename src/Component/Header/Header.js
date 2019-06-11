2bbb04eddbcf92f439307e3659c4f3e8f81ded68
Rafael Boniolo <33812322+rafaelboniolo@users.noreply.github.com>
2019-06-11 02:00:31 -0300

    add favicon

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -15,15 +15,18 @@ export default function Header(props){
 
     return(
             < div style={{display:'flex', backgroundColor:"grey", flex:1, flexDirection:"row", alignItems:'center'}}>
+                
                 <div style={{marginLeft:'10px'}}>
                     <TabMenu model={itemsMenu} activeItem={selectItem} onTabChange={(e) => {
                         setSelectItem(e.value)
                         props.changePage(e.value.label)
                         }}/>
                 </div>
+                
                 <span  style={{display:'flex', flex:6, justifyContent:'center'}}>
                     <h1 style={{color:'black'}}>MapaUTF</h1>
                 </span>
+                
                 <div style={{display:'flex', flex:1, justifyContent:'flex-end'}}>
                     <img src={images[indice]} width={'30%'} height={'35%'}  className="coruja" onClick={()=>{indice === 1 ? setIndice(0):setIndice(1)} }/>
                 </div>
